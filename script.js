b439e36447867312b12ebafe6c3e5ddb8475be06
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-11-18 01:34:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-$(document).ready(function () {
-    $("#submit").click(function startt() {
-        var a = parseInt($("#num").prop("value"));
-        if (a == NaN) {
-            alert("Invalid number");
-            return;
-        } 
-        else if (a >= 4600 && a <= 4700) {
-            document.location.href = "https://businfo.madebyesmel.com/22xd40";
-        }
-        
-        else if (a >= 4500 && a <= 4598) {
-            document.location.href = "https://businfo.madebyesmel.com/21xd40";
-        } 
+$(document).ready(() => {
+	$("#submit").click(function startt() {
+		var a = parseInt($("#num").prop("value"));
+		if (a == NaN) {
+			alert("Invalid number");
+			return;
+		}
+		else if (a >= 4600 && a <= 4700) {
+			document.location.href = "https://businfo.madebyesmel.com/22xd40";
+		}
 
-        else if (a >= 5500 && a <= 5541) {
-            document.location.href = "https://businfo.madebyesmel.com/21xd60";
-        } 
+		else if (a >= 4500 && a <= 4598) {
+			document.location.href = "https://businfo.madebyesmel.com/21xd40";
+		}
 
-        else if (a >= 3275 && a <= 3349) {
-            document.location.href = "https://businfo.madebyesmel.com/20xn40";
-        } 
+		else if (a >= 5500 && a <= 5541) {
+			document.location.href = "https://businfo.madebyesmel.com/21xd60";
+		}
 
-        else if (a >= 4475 && a <= 4499) {
-            document.location.href = "https://businfo.madebyesmel.com/20xd40";
-        } 
+		else if (a >= 3275 && a <= 3349) {
+			document.location.href = "https://businfo.madebyesmel.com/20xn40";
+		}
 
-        else if (a >= 4450 && a <= 4474) {
-            document.location.href = "https://businfo.madebyesmel.com/19xd40";
-        } 
+		else if (a >= 4475 && a <= 4499) {
+			document.location.href = "https://businfo.madebyesmel.com/20xd40";
+		}
 
-        else if (a >= 3200 && a <= 3274) {
-            document.location.href = "https://businfo.madebyesmel.com/19xn40";
-        } 
+		else if (a >= 4450 && a <= 4474) {
+			document.location.href = "https://businfo.madebyesmel.com/19xd40";
+		}
 
-        else if (a >= 5481 && a <= 5492) {
-            document.location.href = "https://businfo.madebyesmel.com/18xde60";
-        } 
+		else if (a >= 3200 && a <= 3274) {
+			document.location.href = "https://businfo.madebyesmel.com/19xn40";
+		}
 
-        else {
-            alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
-        }
-    });
+		else if (a >= 5481 && a <= 5492) {
+			document.location.href = "https://businfo.madebyesmel.com/18xde60";
+		}
+
+		else {
+			alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
+		}
+	});
 });
