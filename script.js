192443c6b5d3bb4d9bbd94aa357eb4734c0ac0ad
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2022-11-18 19:49:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ function startt() {
 		return;
 	}
 	else if (a >= 4600 && a <= 4700) {
-		document.location.href = "https://businfo.madebyesmel.com/22xd40";
+		document.location.href = "/buses/22xd40";
 	}
 
 	else if (a >= 4500 && a <= 4598) {
-		window.location.href = "https://businfo.madebyesmel.com/21xd40";
+		window.location.href = "/buses/21xd40";
 	}
 
 	else if (a >= 5500 && a <= 5541) {
-		document.location.href = "https://businfo.madebyesmel.com/21xd60";
+		document.location.href = "/buses/21xd60";
 	}
 
 	else if (a >= 3275 && a <= 3349) {
-		document.location.href = "https://businfo.madebyesmel.com/20xn40";
+		document.location.href = "/buses/20xn40";
 	}
 
 	else if (a >= 4475 && a <= 4499) {
-		document.location.href = "https://businfo.madebyesmel.com/20xd40";
+		document.location.href = "/buses/20xd40";
 	}
 
 	else if (a >= 4450 && a <= 4474) {
-		document.location.href = "https://businfo.madebyesmel.com/19xd40";
+		document.location.href = "/buses/19xd40";
 	}
 
 	else if (a >= 3200 && a <= 3274) {
-		document.location.href = "https://businfo.madebyesmel.com/19xn40";
+		document.location.href = "/buses/19xn40";
 	}
 
 	else if (a >= 5481 && a <= 5492) {
-		document.location.href = "https://businfo.madebyesmel.com/18xde60";
+		document.location.href = "/buses/18xde60";
 	}
 
 	else {
