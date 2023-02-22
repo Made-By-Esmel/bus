2b03ebb1b7bfd2045761e2a5463230785fe314ef
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-22 18:53:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,34 @@
 function startt() {
-	var a = parseInt($("#numbers").prop("value"));
-	if (a == NaN) {
-		alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
-		return;
-	}
-	else if (a >= 4600 && a <= 4700) {
-		document.location.href = "/buses/wmata/22xd40";
-	}
+  var a = parseInt($("#numbers").prop("value"));
+  if (isNaN(a)) {
+    alertInvalidNumber();
+    return;
+  }
 
-	else if (a >= 4500 && a <= 4598) {
-		window.location.href = "/buses/wmata/21xd40";
-	}
+  var busNumbers = {
+    '4600-4700': '/buses/wmata/22xd40',
+    '4500-4598': '/buses/wmata/21xd40',
+    '5500-5541': '/buses/wmata/21xd60',
+    '3275-3349': '/buses/wmata/20xn40',
+    '4475-4499': '/buses/wmata/20xd40',
+    '4450-4474': '/buses/wmata/19xd40',
+    '3200-3274': '/buses/wmata/19xn40',
+    '5481-5492': '/buses/wmata/18xde60',
+    '5747-5770': '/buses/rideon/glfa'
+  };
 
-	else if (a >= 5500 && a <= 5541) {
-		document.location.href = "/buses/wmata/21xd60";
-	}
+  for (var range in busNumbers) {
+    var start = parseInt(range.split('-')[0]);
+    var end = parseInt(range.split('-')[1]);
+    if (a >= start && a <= end) {
+      document.location.href = busNumbers[range];
+      return;
+    }
+  }
 
-	else if (a >= 3275 && a <= 3349) {
-		document.location.href = "/buses/wmata/20xn40";
-	}
+  alertInvalidNumber();
+}
 
-	else if (a >= 4475 && a <= 4499) {
-		document.location.href = "/buses/wmata/20xd40";
-	}
-
-	else if (a >= 4450 && a <= 4474) {
-		document.location.href = "/buses/wmata/19xd40";
-	}
-
-	else if (a >= 3200 && a <= 3274) {
-		document.location.href = "/buses/wmata/19xn40";
-	}
-
-	else if (a >= 5481 && a <= 5492) {
-		document.location.href = "/buses/wmata/18xde60";
-	}
-
-	else if (a >= 5747 && a <= 5770) {
-		document.location.href = "/buses/rideon/glfa";
-	}
-
-
-
-	else {
-		alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
-	}
+function alertInvalidNumber() {
+  alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
 }
