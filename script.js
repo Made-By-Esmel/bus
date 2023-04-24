eb91240824707d77b0ce877f5024b7b7cf36d111
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-04-24 12:14:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 function start() {
-  var a = parseInt($("#numbers").prop("value"));
-  if (isNaN(a)) {
+  const numberInput = document.querySelector("#numbers").value;
+  const redirectUrl = getRedirectUrl(numberInput);
+  if (redirectUrl) {
+    document.location.href = redirectUrl;
+  } else {
     alertInvalidNumber();
-    return;
   }
-
-  var busNumbers = {
-    '4600-4700': '/buses/wmata/22xd40',
-    '4500-4598': '/buses/wmata/21xd40',
-    '5500-5541': '/buses/wmata/21xd60',
-    '3275-3349': '/buses/wmata/20xn40',
-    '4475-4499': '/buses/wmata/20xd40',
-    '4450-4474': '/buses/wmata/19xd40',
-    '3200-3274': '/buses/wmata/19xn40',
-    '5481-5492': '/buses/wmata/18xde60',
-    '5747-5770': '/buses/rideon/glfa'
-  };
-
-  for (var range in busNumbers) {
-    var start = parseInt(range.split('-')[0]);
-    var end = parseInt(range.split('-')[1]);
-    if (a >= start && a <= end) {
-      document.location.href = busNumbers[range];
-      return;
-    }
-  }
-
-  alertInvalidNumber();
 }
+
+function getRedirectUrl(numberInput) {
+  if (numberInput >= 4600 && numberInput <= 4700) {
+    return "/buses/wmata/22xd40";
+  } else if (numberInput >= 4500 && numberInput <= 4598) {
+    return "/buses/wmata/21xd40";
+  } else if (numberInput >= 5500 && numberInput <= 5541) {
+    return "/buses/wmata/21xd60";
+  } else if (numberInput >= 3275 && numberInput <= 3349) {
+    return "/buses/wmata/20xn40";
+  } else if (numberInput >= 4475 && numberInput <= 4499) {
+    return "/buses/wmata/20xd40";
+  } else if (numberInput >= 4450 && numberInput <= 4474) {
+    return "/buses/wmata/19xd40";
+  } else if (numberInput >= 3200 && numberInput <= 3274) {
+    return "/buses/wmata/19xn40";
+  } else if (numberInput >= 5481 && numberInput <= 5492) {
+    return "/buses/wmata/18xde60";
+  } else if (numberInput >= 5747 && numberInput <= 5770) {
+    return "/buses/rideon/11glfa";
+  } else {
+    return null;
+  }
+}
+
 
 function alertInvalidNumber() {
   alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
