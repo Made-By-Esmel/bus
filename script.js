f3e90ad2af252abd6ae004e6d8e21d4a3748fdcd
Andrew Frykman <andrewfrykman123@icloud.com>
2023-04-26 13:26:36 -0400

    Worked out the kinks

--- script.js
+++ script.js
@@ -37,7 +37,57 @@
 //   return null;
 // }
 
-window.onload = function () {
-  alert("We are working very hard on giving you the best visit. However, this service has been taken down to complete some important edits, we believe you will like the new UI even better than you do now! See you soon!");
-  window.location = "https://wmata.info";
+const form = document.querySelector('form');
+const numbersInput = document.querySelector('#numbers');
+const wmataRadio = document.querySelector('input[value="wmata"]');
+const rideonRadio = document.querySelector('input[value="rideon"]');
+
+const wmataRanges = [
+  { min: 4600, max: 4700, url: '/buses/wmata/22xd40' },
+  { min: 4500, max: 4598, url: '/buses/wmata/21xd40' },
+  { min: 5500, max: 5541, url: '/buses/wmata/21xd60' },
+  { min: 3275, max: 3349, url: '/buses/wmata/20xn40' },
+  { min: 4475, max: 4499, url: '/buses/wmata/20xd40' },
+  { min: 4450, max: 4474, url: '/buses/wmata/19xd40' },
+  { min: 3200, max: 3274, url: '/buses/wmata/19xn40' },
+  { min: 5481, max: 5492, url: '/buses/wmata/18xde60' },
+];
+
+const rideonRanges = [
+  { min: 5747, max: 5770, url: '/buses/rideon/11glfa' },
+];
+
+function validateInput() {
+  const inputNumber = parseInt(numbersInput.value);
+  let isValid = false;
+  let redirectUrl = '';
+
+  if (wmataRadio.checked) {
+    for (let range of wmataRanges) {
+      if (inputNumber >= range.min && inputNumber <= range.max) {
+        isValid = true;
+        redirectUrl = range.url;
+        break;
+      }
+    }
+  } else if (rideonRadio.checked) {
+    for (let range of rideonRanges) {
+      if (inputNumber >= range.min && inputNumber <= range.max) {
+        isValid = true;
+        redirectUrl = range.url;
+        break;
+      }
+    }
+  }
+
+  if (isValid) {
+    window.location.href = redirectUrl;
+  } else {
+    alert('This is an invalid bus identification number or is pre-2018 which isn\'t fully supported yet.');
+  }
 }
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  validateInput();
+});
