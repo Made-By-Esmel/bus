62026c93b34161236d5f15c773af7f0bb7f69412
Andrew Frykman <andrewfrykman123@icloud.com>
2023-04-26 14:07:24 -0400

    ...

--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-// document.querySelector("#submitbutton").removeEventListener("click");
-// document.querySelector("#submitbutton").addEventListener("click", function (event) {
-//   const numberInput = document.querySelector("#numbers").value;
-//   const redirectUrl = getRedirectUrl(numberInput);
-
-//   if (numberInput === "") {
-//     return;
-//   }
-
-//   if (redirectUrl) {
-//     window.open("https://bus.wmata.info" + redirectUrl, "_blank");
-//   } else {
-//     alert("This is an invalid bus identification number or is pre-2018 which isn't supported yet.");
-//     event.preventDefault();
-//   }
-// });
-
-// const redirectUrls = {
-//   "4600-4700": "/buses/wmata/22xd40",
-//   "4500-4598": "/buses/wmata/21xd40",
-//   "5500-5541": "/buses/wmata/21xd60",
-//   "3275-3349": "/buses/wmata/20xn40",
-//   "4475-4499": "/buses/wmata/20xd40",
-//   "4450-4474": "/buses/wmata/19xd40",
-//   "3200-3274": "/buses/wmata/19xn40",
-//   "5481-5492": "/buses/wmata/18xde60",
-//   "5747-5770": "/buses/rideon/11glfa"
-// };
-
-// function getRedirectUrl(numberInput) {
-//   for (const range in redirectUrls) {
-//     const [start, end] = range.split("-");
-//     if (numberInput >= start && numberInput <= end) {
-//       return redirectUrls[range];
-//     }
-//   }
-//   return null;
-// }
-
 const form = document.querySelector('form');
 const numbersInput = document.querySelector('#numbers');
 const wmataRadio = document.querySelector('input[value="wmata"]');
 const rideonRadio = document.querySelector('input[value="rideon"]');
-const art = document.querySelector('input[value="rideon"]');
+const art = document.querySelector('input[value="art"]');
 
 const wmataRanges = [
   { min: 4600, max: 4700, url: '/buses/wmata/22xd40' },
